a51ba790b8486e8c772c8e64ca49083923b16a6e
Starr Z. Chen <chenstarr@gmail.com>
2015-11-13 15:56:53 -0500

    Martha seeds, pin pages show images, board pages show pins w/ images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+Board.destroy_all
 Pin.destroy_all
 
-Pin.create(title: "Puppeh", image_url: "http://cdn.sheknows.com/articles/2013/04/Puppy_2.jpg")
+martha = User.create!(email: 'martha@stewart.com', password: 'topsecret', password_confirmation: 'topsecret')
+
+sweaters = martha.boards.create!(title: "Good Sweaters for Prison")
+cableknit = sweaters.pins.create!(title: "Cable Knit", image_url: "http://leopardprintcardigan.net/wp-content/uploads/2010/08/cable-knit-cardigan-sweaters.jpg")
+fairisle = sweaters.pins.create!(title: "Fair Isle", image_url: "http://in1.ccio.co/FE/wD/Z8/47f627515bb601505877c38005b8716b.jpg?iw=300")
+uglyxmas = sweaters.pins.create!(title: "Ugly Christmas", image_url: "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=124059940")
