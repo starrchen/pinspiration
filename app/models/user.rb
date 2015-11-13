d0145f5e3184d78c8970573323780083be1ac6ff
Starr Z. Chen <chenstarr@gmail.com>
2015-11-13 13:30:08 -0500

    Add board model and migration, ref user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :pins
+  has_many :pins, through: :boards
+  has_many :boards
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
