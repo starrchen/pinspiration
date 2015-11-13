b505c585c1815bd6ee8aab3c7cb59d476a9ad8db
Starr Z. Chen <chenstarr@gmail.com>
2015-11-13 14:01:04 -0500

    New routes: nested boards in users, pins in boards

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pins#index'
 
-  resources :pins do
-  end
+      resources :pins do
+      end
 
+      resources :boards do
+        resources :pins
+      end
+
+      resources :users do
+        resources :boards
+      end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
